dd2e06bea8a3b5f18dc97bb0199e332071c4147f
awidawad <austinw125@gmail.com>
2014-06-26 16:53:52 -0400

    Changes for Omni

--- omni_t6spr.mk
+++ omni_t6spr.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -8,7 +8,8 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+#implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
@@ -21,15 +22,23 @@
 # lines, full and maguro, hence its name.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Get the long list of APNs
+PRODUCT_COPY_FILES := 
+device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Inherit from m7ul device
+# Inherit from those products. Most specific first.
+$(call inherit-product, 
+$(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from t6spr device
 $(call inherit-product, device/htc/t6spr/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_t6spr
+PRODUCT_NAME := omni_t6spr
 PRODUCT_DEVICE := t6spr
 PRODUCT_BRAND := htc
-PRODUCT_MANUFACTURER := HTC
+PRODUCT_MANUFACTURER := htc
 PRODUCT_MODEL := One Max
