7d5524e8515e9bd82c20914c8e8ee6e806db8a2c
Austin Widner <austinw125@gmail.com>
2014-06-26 15:59:30 -0500

    omni_t6spr.mk: fix derps from bringup

--- omni_t6spr.mk
+++ omni_t6spr.mk
@@ -23,12 +23,10 @@
 #
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := 
-device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, 
-$(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
